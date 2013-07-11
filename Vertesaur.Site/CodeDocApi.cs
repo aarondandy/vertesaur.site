2adba52acb595c83714a5171987951a5d622c3dc
Aaron Dandy <aaron.dandy@live.com>
2013-07-10 22:28:10 -0600

    started the code doc service

---
+++ Vertesaur.Site/CodeDocApi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Net;
+using System.Web;
+using DandyDoc.CRef;
+using DandyDoc.CodeDoc;
+using ServiceStack.Common.Web;
+
+namespace Vertesaur.Site
+{
+
+    public class CodeDocCRefRequest
+    {
+        public string CRef { get; set; }
+    }
+
+    public class CodeDocApi
+    {
+
+        public CodeDocRepositories Repositories { get; set; }
+
+        public CodeDocSimpleMember Any(CodeDocCRefRequest request) {
+            if (request == null) throw new ArgumentNullException("request");
+            if (String.IsNullOrEmpty(request.CRef)) throw new ArgumentException("Code reference (CRef) not provided.", "request");
+            Contract.EndContractBlock();
+
+            var model = Repositories.GetModel(new CRefIdentifier(request.CRef));
+            if (model == null)
+                throw new HttpError(HttpStatusCode.NotFound, "Documentation model could not be found.");
+            return model;
+        }
+
+    }
+}
---
+++ Vertesaur.Site/CodeDocRepositories.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DandyDoc.CRef;
+using DandyDoc.CodeDoc;
+
+namespace Vertesaur.Site
+{
+    public class CodeDocRepositories
+    {
+
+        static CodeDocRepositories() {
+            
+        }
+
+        public CodeDocSimpleMember GetModel(CRefIdentifier cRef) {
+            throw new NotImplementedException();
+        }
+
+    }
+}
